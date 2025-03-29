f6f4d63e19541d45d03585b4cc0dee951c654c1c
Chivito1028 <Martin1028v@gmail.com>
2025-03-28 17:18:09 -0700

    Calander
    Added calander and a couple of events to test it

---
+++ frontend/src/pages/Calander.js
@@ -0,0 +1,14 @@
+import React from "react";
+import EventCalander from "../components/EventCalander";
+
+const Calander = () => {
+  return (
+    <div>
+      <h1>Calander Page</h1>
+      <p>Browse and manage events here.</p>
+      <EventCalander/>
+    </div>
+  );
+};
+
+export default Calander;
