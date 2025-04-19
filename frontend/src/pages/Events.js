8187ce0161ff8e9623b067c14a40be0ea54f5965
Raymond Cromwell <raymondcromwell2@gmail.com>
2025-04-19 21:11:19 +0000

    Remove files that are now in .gitignore from tracking

--- frontend/src/pages/Events.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import EventSearch from "../components/EventSearch";
-
-const Events = ({ isLoaded, searchQuery, locationQuery }) => {
-  return <EventSearch isLoaded={isLoaded} searchQuery={searchQuery} locationQuery={locationQuery} />;
-};
-
-export default Events;
-
--- frontend/webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    devServer: {
-      client: {
-        webSocketURL: false, // Disable WebSockets
-      },
-    },
-  };
